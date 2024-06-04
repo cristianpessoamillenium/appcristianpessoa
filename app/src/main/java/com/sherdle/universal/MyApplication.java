90463f7af6d27148c5fb0f5ff78dfeef4c29ecd0
Cristian Paiva <cristianpaiva@MacBook-Pro-de-Cristian.local>
2024-06-04 12:10:23 -0300

    Initial commit

---
+++ app/src/main/java/com/sherdle/universal/MyApplication.java
@@ -0,0 +1,24 @@
+package com.sherdle.universal;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MyApplication extends App {
+    private static AppOpenManager appOpenManager;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MobileAds.initialize(
+                this,
+                new OnInitializationCompleteListener() {
+                    @Override
+                    public void onInitializationComplete(InitializationStatus initializationStatus) {
+                    }
+                });
+
+        appOpenManager = new AppOpenManager(this);
+
+    }
+}
